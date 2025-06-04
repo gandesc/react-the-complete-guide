be07e35f45010c456f68b2864a968d144e424c2a
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2025-06-04 12:53:04 +0300

    dynamic values for html attributes

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,4 +1,7 @@
+import reactImg from './assets/react-core-concepts.png';
+
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
 function genRandomInt(max) {
     return Math.floor(Math.random() * (max + 1));
 }
@@ -8,7 +11,7 @@ function Header() {
 
     return (
         <header>
-            <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
+            <img src={reactImg} alt="Stylized atom"/>
             <h1>React Essentials</h1>
             <p>
                 {description} React concepts you will need for almost any app you are
