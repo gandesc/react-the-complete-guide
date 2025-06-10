80973aa4764d8a15bc29e1843d40ff8487030c28
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2025-06-10 12:52:19 +0300

    adding state derived content

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState} from "react";
+import {useState} from "react";
 
-import {CORE_CONCEPTS} from "./data.js";
+import {CORE_CONCEPTS, EXAMPLES} from "./data.js";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 
 
 function App() {
-    const [selectedTopic, setSelectedTopic ] = useState('Please click a button');
-    
+    const [selectedTopic, setSelectedTopic] = useState('components');
+
     function handleClick(selectedButton) {
         setSelectedTopic(selectedButton);
         console.log(selectedTopic)
@@ -41,6 +41,11 @@ function App() {
                         <TabButton onSelect={() => handleClick('props')}>Props</TabButton>
                         <TabButton onSelect={() => handleClick('state')}>State</TabButton>
                     </menu>
+                    <div id="tab-content">
+                        <h3>{EXAMPLES[selectedTopic].title}</h3>
+                        <p>{EXAMPLES[selectedTopic].description}</p>
+                        <pre>{EXAMPLES[selectedTopic].code}</pre>
+                    </div>
                     {selectedTopic}
                 </section>
             </main>
